a0df0519624f605cea7773ba75937179bf190da9
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2023-03-25 18:40:24 -0400

    Finished conversions between oz, mL, and L

--- main.py
+++ main.py
@@ -81,29 +81,33 @@ drop = OptionMenu(main_menu, clicked, *options)
 drop.pack()
 
 global allDrinkEntries
-def display_total_saved(value, measurement):
-    print(value)
-    print(measurement)
+allDrinkEntries= []
+def display_total_entries(value, measurement):
+    global allDrinkEntries
     drinkEntry = [None] * 3
     if measurement == 'oz':
         print("it's oz")
-        drinkEntry[0] = value
-        drinkEntry[1] = 0 #not finished
-        drinkEntry[2] = 0 #not finished
+        drinkEntry[0] = str(int(value))
+        drinkEntry[1] = str(int(value) * 29.57353) #milliliters = fluid ounces × 29.57353
+        drinkEntry[2] = str(int(value)/34) #L = oz/33.814
+
     elif measurement == 'mL':
         print("it's mL")
-        drinkEntry[0] = 0 #not finished
-        drinkEntry[1] = value
-        drinkEntry[2] = 0 #not finished
+        drinkEntry[0] = str(int(value)/29.57353) #oz = mL/29.57353
+        drinkEntry[1] = str(int(value))
+        drinkEntry[2] = str(int(value)/1000) #L = mL/1000
     else:
         print("it's L")
-        drinkEntry[0] = 0 #not finished
-        drinkEntry[1] = 0 #not finished
-        drinkEntry[2] = value
+        drinkEntry[0] = str(int(value) * 33.814) #oz = L * 33.814
+        drinkEntry[1] = str(int(value) * 1000) #ml = L * 1000
+        drinkEntry[2] = str(int(value))
 
     allDrinkEntries.append(drinkEntry)
+    print(allDrinkEntries)
 
-
+def display_total_saved(value, measurement):
+    print(value)
+    print(measurement)
 
 
 def validate_water_entry():
@@ -111,6 +115,7 @@ def validate_water_entry():
     entry.delete(0, END)
     if re.match("^(0|[1-9]\d*)?(\.\d+)?(?<=\d)$", value):
         display_total_saved(value, clicked.get())
+        display_total_entries(value, clicked.get())
         clicked.set("oz")
         print("Yellooooo")
     else:
