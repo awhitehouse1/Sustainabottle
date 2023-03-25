2c36ea59052eb30af9d8e6ca674032508bd7e52c
Ashley Whitehouse <uku9qn@virginia.edu>
2023-03-25 17:18:09 -0400

    fixed nav bar

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
-import tkinter as tk
+# Import the required libraries
+from tkinter import *
+from tkinter import font
 
-# Create the main window
-root = tk.Tk()
+# Create an instance of tkinter frame or window
+root = Tk()
 
-# Set the window title
-root.title("Main Menu")
-
-# Set the window size
+# Set the size of the window
 root.geometry("432x600")
 root.resizable("False", "False")
 
-main_frame = tk.Frame(root)
-stats_frame = tk.Frame(root)
-settings_frame = tk.Frame(root)
+# Create two frames in the window
+main_menu = Frame(root)
+stats = Frame(root)
+settings = Frame(root)
 
 
+# Define a function for switching the frames
 def change_to_main():
-    main_frame.pack(fill='both', expand=1)
-    settings_frame.pack_forget()
-    stats_frame.pack_forget()
+    main_menu.pack(fill='both', expand=1)
+    stats.pack_forget()
+    settings.pack_forget()
 
 
 def change_to_stats():
-    stats_frame.pack(fill='both', expand=1)
-    main_frame.pack_forget()
-    settings_frame.pack_forget()
+    stats.pack(fill='both', expand=1)
+    main_menu.pack_forget()
+    settings.pack_forget()
 
 def change_to_settings():
-    settings_frame.pack(fill='both', expand=1)
-    main_frame.pack_forget()
-    stats_frame.pack_forget()
+    settings.pack(fill='both', expand=1)
+    main_menu.pack_forget()
+    stats.pack_forget()
 
+# Add a heading logo in the frames
+label1 = Label(main_menu, text="in main", foreground="green3")
+label1.place(x=-5,y=0)
 
-main_btn = tk.Button(root, text="Main", width=20, height=7, command=change_to_main())
-main_btn.pack(side=tk.LEFT)
+label2 = Label(stats, text="in stats", foreground="blue")
+label2.pack(side=LEFT)
 
-stats_btn = tk.Button(root, text="Stats", width=20, height=7, command=change_to_stats())
-stats_btn.pack(side=tk.LEFT)
+label3 = Label(settings, text="in settings")
+label3.pack(side=LEFT)
 
-settings_btn = tk.Button(root, text="Settings", width=20, height=7, command=change_to_settings())
-settings_btn.pack(side=tk.LEFT)
+# Add a button to switch between two frames
+main_menu_btn = Button(root, text="Main Menu", height=7, width=20, command=change_to_main)
+main_menu_btn.place(x=0, y=520)
 
+stats_btn = Button(root, text="Stats", height=7, width=20, command=change_to_stats)
+stats_btn.place(x=145, y=520)
 
+settings_btn = Button(root, text="Settings", height=7, width=20, command=change_to_settings)
+settings_btn.place(x=290, y=520)
 
 root.mainloop()
