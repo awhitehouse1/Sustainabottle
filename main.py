89b6ec3a475eb464f3e2cd017cc22e48efbb217f
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2023-03-25 15:12:26 -0400

    Switched to tkinter.

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from kivy.app import App
-from kivy.properties import StringProperty
+import tkinter as tk
+import datetime as dt
+import time
+#Import pandas and matplotlib to use code.
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.scatter import Scatter
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
+# Tkinter  tutorial link: https://realpython.com/python-gui-tkinter/
+# Tkinter checkbox link: https://pythonbasics.org/tkinter-checkbox/
 
+window = tk.Tk()
 
-
-class BoxLayoutExample(BoxLayout):
-    my_text = StringProperty("1")
-    text_input_str = StringProperty("1")
-    def on_button_click(self):
-        print("button clicked")
-    def on_text_validate(self, widget):
-        self.text_input_str = widget.text
-
-class MainWidget(Widget):
-    pass
-
-class WaterApp(App):
-    pass
-
-WaterApp().run()
+window.title("Water App")
+window.geometry("800x600")
+window.config(bg="white")
+window.mainloop()
+# Menu Code - Start
+# - https://www.tutorialspoint.com/python/tk_menu.htm - menus
+# - https://www.pythontutorial.net/tkinter/tkinter-menu/ - submenus
+def donothing():  # Placeholder Function
+    filewin = tk.Toplevel(window)
+    button = tk.Button(filewin, text="Do nothing button")
+    button.pack()
