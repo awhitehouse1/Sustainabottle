0f637775260912e744b62d6b4b5736ba7d155748
Ashley Whitehouse <uku9qn@virginia.edu>
2023-03-25 14:04:17 -0400

    added text input that enters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.properties import StringProperty
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -10,8 +11,12 @@ from kivy.uix.widget import Widget
 
 
 class BoxLayoutExample(BoxLayout):
+    my_text = StringProperty("1")
+    text_input_str = StringProperty("1")
     def on_button_click(self):
         print("button clicked")
+    def on_text_validate(self, widget):
+        self.text_input_str = widget.text
 
 class MainWidget(Widget):
     pass
