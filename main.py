34a2f224d4ed8de81653c4921245d929c853cd77
Ashley Whitehouse <uku9qn@virginia.edu>
2023-03-25 17:43:18 -0400

    merged changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import the required libraries
 from tkinter import *
 from tkinter import font
+from PIL import Image,ImageTk
+from PIL.Image import Resampling
 
 # Create an instance of tkinter frame or window
 root = Tk()
@@ -8,6 +10,7 @@ root = Tk()
 # Set the size of the window
 root.geometry("432x600")
 root.resizable("False", "False")
+root.title("Water App")
 
 # Create two frames in the window
 main_menu = Frame(root)
@@ -27,14 +30,67 @@ def change_to_stats():
     main_menu.pack_forget()
     settings.pack_forget()
 
+
 def change_to_settings():
     settings.pack(fill='both', expand=1)
     main_menu.pack_forget()
     stats.pack_forget()
 
+
 # Add a heading logo in the frames
-label1 = Label(main_menu, text="in main", foreground="green3")
-label1.place(x=-5,y=0)
+
+# MAIN MENU
+# Add the image
+img = Image.open("bottle.jpg")
+img = img.resize((200, 200), Resampling.LANCZOS)
+img = ImageTk.PhotoImage(img)
+label_image = Label(main_menu, image=img)
+label_image.image = img
+label_image.place(x=115, y=100)
+
+askTaskName = Label(main_menu, text="How much water did you drink since your last entry?", bg="white")
+entry = Entry(main_menu)
+
+askTaskName.pack()
+entry.pack()
+
+# Dropdown menu options
+options = [
+    "oz",
+    "mL",
+    "L"
+]
+
+# datatype of menu text
+clicked = StringVar()
+
+# initial menu text
+clicked.set("oz")
+
+# Create Dropdown menu
+drop = OptionMenu(main_menu, clicked, *options)
+drop.pack()
+
+
+def display_total_saved(value, measurement):
+    print(measurement)
+
+
+def validate_water_entry():
+    value = entry.get()
+    entry.delete(0, END)
+    if value.isnumeric() | value.isdecimal():  # Does not account for floats yet
+        display_total_saved(value, clicked.get())
+        clicked.set("oz")
+        print("Yellooooo")
+    else:
+        # Display an error label here
+        print("HI")
+
+
+button = Button(main_menu, text="Enter", command=validate_water_entry)  # Can be repurposed into
+button.pack()
+
 
 label2 = Label(stats, text="in stats", foreground="blue")
 label2.pack(side=LEFT)
